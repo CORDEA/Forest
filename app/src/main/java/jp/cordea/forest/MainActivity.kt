7e21585d9e319371ff7ee5107625bb88e69e055f
Yoshihiro Tanaka <contact@cordea.jp>
2020-04-04 20:35:14 +0900

    update layout

--- app/src/main/java/jp/cordea/forest/MainActivity.kt
+++ app/src/main/java/jp/cordea/forest/MainActivity.kt
@@ -3,31 +3,41 @@ package jp.cordea.forest
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
+import androidx.ui.core.Alignment
 import androidx.ui.core.Text
 import androidx.ui.core.setContent
+import androidx.ui.foundation.Clickable
+import androidx.ui.foundation.VerticalScroller
+import androidx.ui.layout.*
 import androidx.ui.material.MaterialTheme
-import androidx.ui.tooling.preview.Preview
+import androidx.ui.material.ripple.Ripple
+import androidx.ui.unit.dp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MaterialTheme {
-                Greeting("Android")
+                VerticalScroller(modifier = LayoutHeight.Fill + LayoutWidth.Fill) {
+                    Column {
+                        (0..20).forEach { Item(it.toString()) }
+                    }
+                }
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview
-@Composable
-fun DefaultPreview() {
-    MaterialTheme {
-        Greeting("Android")
+fun Item(text: String) {
+    Ripple(bounded = true) {
+        Clickable(onClick = {}) {
+            Container(padding = EdgeInsets(16.dp)) {
+                Column(modifier = LayoutAlign.CenterLeft) {
+                    Text(text = text)
+                    Text(text = text)
+                }
+            }
+        }
     }
 }
