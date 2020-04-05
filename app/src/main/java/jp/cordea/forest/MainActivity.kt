8de9c3efb136a327441d5ff496a1241966df9ea2
Yoshihiro Tanaka <contact@cordea.jp>
2020-04-05 17:05:10 +0900

    list apps

--- app/src/main/java/jp/cordea/forest/MainActivity.kt
+++ app/src/main/java/jp/cordea/forest/MainActivity.kt
@@ -3,7 +3,6 @@ package jp.cordea.forest
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
-import androidx.ui.core.Alignment
 import androidx.ui.core.Text
 import androidx.ui.core.setContent
 import androidx.ui.foundation.Clickable
@@ -14,13 +13,15 @@ import androidx.ui.material.ripple.Ripple
 import androidx.ui.unit.dp
 
 class MainActivity : AppCompatActivity() {
+    private val resolver by lazy { AppResolver(this) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MaterialTheme {
                 VerticalScroller(modifier = LayoutHeight.Fill + LayoutWidth.Fill) {
                     Column {
-                        (0..20).forEach { Item(it.toString()) }
+                        resolver.resolve().forEach { Item(it) }
                     }
                 }
             }
@@ -29,13 +30,12 @@ class MainActivity : AppCompatActivity() {
 }
 
 @Composable
-fun Item(text: String) {
+fun Item(app: AppResolver.App) {
     Ripple(bounded = true) {
-        Clickable(onClick = {}) {
+        Clickable(onClick = { app.launch() }) {
             Container(padding = EdgeInsets(16.dp)) {
-                Column(modifier = LayoutAlign.CenterLeft) {
-                    Text(text = text)
-                    Text(text = text)
+                Column(modifier = LayoutWidth.Fill) {
+                    Text(text = app.name)
                 }
             }
         }
